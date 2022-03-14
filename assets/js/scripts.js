a5d1d08ebc45eb772856fdf24f3f0c1ed6390d83
Benie <davinci.monalissa@gmail.com>
2022-03-14 14:25:46 +0300

    Add many small changes to the css, js, and html for general page responsiveness and correctness.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,32 +1,113 @@
 $(document).ready(function(){
-  $("#developmentImage").click(function(){
-    $("#developmentImage").slideDown('1500').hide('1000');
+  $("#devIcon").click(function(){
+    $("#devIcon").slideDown('1500').hide('1000');
     $("#development").show('1500');
   });
   $("#development").click(function(){
     $("#development").slideUp('1500');
-    $("#developmentImage").slideDown('1500');
+    $("#devIcon").slideDown('1500');
   });
 });
 
 $(document).ready(function(){
-  $("#designImage").click(function(){
-    $("#designImage").slideDown('1500').hide('1000');
+  $("#designIcon").click(function(){
+    $("#designIcon").slideDown('1500').hide('1000');
     $("#design").show('1500');
   });
   $("#design").click(function(){
     $("#design").slideUp('1500');
-    $("#designImage").slideDown('1500');
+    $("#designIcon").slideDown('1500');
   });
 });
 
 $(document).ready(function(){
-  $("#productImage").click(function(){
-    $("#productImage").slideDown('1500').hide('1000');
+  $("#productIcon").click(function(){
+    $("#productIcon").slideDown('1500').hide('1000');
     $("#product").show('1500');
   });
   $("#product").click(function(){
     $("#product").slideUp('1500');
-    $("#productImage").slideDown('1500');
+    $("#productIcon").slideDown('1500');
   });
+});
+
+
+$(function() {
+	$("#project1").css("opacity","1");
+	$("#project1").hover(function() {
+		$(this).stop().animate({ opacity: 0.8 }, "fast");
+	},
+	function() {
+		$(this).stop().animate({ opacity: 1 }, "fast");
+	});
+});
+
+$(function() {
+	$("#project2").css("opacity","1");
+	$("#project2").hover(function() {
+		$(this).stop().animate({ opacity: 0.8 }, "fast");
+	},
+	function() {
+		$(this).stop().animate({ opacity: 1 }, "fast");
+	});
+});
+
+$(function() {
+	$("#project3").css("opacity","1");
+	$("#project3").hover(function() {
+		$(this).stop().animate({ opacity: 0.8 }, "fast");
+	},
+	function() {
+		$(this).stop().animate({ opacity: 1 }, "fast");
+	});
+});
+
+$(function() {
+	$("#project4").css("opacity","1");
+	$("#project4").hover(function() {
+		$(this).stop().animate({ opacity: 0.8 }, "fast");
+	},
+	function() {
+		$(this).stop().animate({ opacity: 1 }, "fast");
+	});
+});
+
+$(function() {
+	$("#project5").css("opacity","1");
+	$("#project5").hover(function() {
+		$(this).stop().animate({ opacity: 0.8 }, "fast");
+	},
+	function() {
+		$(this).stop().animate({ opacity: 1 }, "fast");
+	});
+});
+
+$(function() {
+	$("#project6").css("opacity","1");
+	$("#project6").hover(function() {
+		$(this).stop().animate({ opacity: 0.8 }, "fast");
+	},
+	function() {
+		$(this).stop().animate({ opacity: 1 }, "fast");
+	});
+});
+
+$(function() {
+	$("#project7").css("opacity","1");
+	$("#project7").hover(function() {
+		$(this).stop().animate({ opacity: 0.8 }, "fast");
+	},
+	function() {
+		$(this).stop().animate({ opacity: 1 }, "fast");
+	});
+});
+
+$(function() {
+	$("#project8").css("opacity","1");
+	$("#project8").hover(function() {
+		$(this).stop().animate({ opacity: 0.8 }, "fast");
+	},
+	function() {
+		$(this).stop().animate({ opacity: 1 }, "fast");
+	});
 });
